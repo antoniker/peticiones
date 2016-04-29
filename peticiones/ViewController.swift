189dcd3f3c94405af8f12aecc6f23fbee8f476f2
antoniker <jos_mustang@hotmail.com>
2016-04-29 12:17:14 -0500

    Initial Commit

---
+++ peticiones/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  peticiones
+//
+//  Created by Ing. José Antonio Franco Cortés on 29/04/16.
+//  Copyright © 2016 xquared. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
